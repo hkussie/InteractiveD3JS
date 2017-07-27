5a804b24bc35badd620d9314c4467947870eb720
hkussie <hkussie@gmail.com>
2017-07-27 13:52:41 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+(function () {
+	var width = 500;
+	var height = 500;
+
+	var svg = d3.select("#chart")
+	   .append("svg")
+	   .attr("height", height)
+	   .attr("width", width)
+	   .append("g")
+	   .attr("transform", "translate(0, 0)")
+/*
+use d3 force to initiate movement amoungst the bubbles
+*/
+	var simulation = d3.forceSimulation()   
+
+	d3.queue()
+	  .defer(d3.csv, "Data.csv")
+	  .await(ready)
+
+	function ready(error, datapoints) {
+
+	  var circles = svg.selectAll(".artist")
+	    .data(datapoints)
+	    .enter().append("circle")
+	    .attr("class", "artist")
+	    .attr("r", 10)
+	    .attr("fill", "purple")
+
+	  simulation.nodes(datapoints)
+	  
+	  function ticked() {
+	  	circles
+	  	  .attr("cx", function (d) {
+	  	  	return d.x
+	  	  })
+	  	  .attr("cy", function (d) {
+	  	  	return d.y
+	  	  })
+	  }
+
+	}    
+
+})();
