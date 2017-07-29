d2126ba21524988f3b31be91777cb3a3c39e75ff
hkussie <hkussie@gmail.com>
2017-07-29 00:54:17 -0700

    additions to main script file

--- script.js
+++ script.js
@@ -9,6 +9,20 @@
 	   .append("g")
 	   .attr("transform", "translate(0, 0)")
 
+	var defs = svg.append("defs");
+	
+	defs.append("pattern")
+	  .attr("id", "America")
+	  .attr("height", "100%")
+	  .attr("width", "100%")
+	  .attr("patternContentUnits", "objectBoundingBox")
+	  .append("image")
+	  .attr("height", 1)
+	  .attr("width", 1)
+	  .attr("preserveAspectRatio", "none")
+	  .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+	  .attr("xlink:href", "America.png")   
+
 	var radiusScale = d3.scaleSqrt().domain([0.6, 16.7]).range([10, 80])   
 /*
 Use d3 force to initiate movement amoungst the bubbles.
@@ -32,14 +46,39 @@ which will determine how the circles will move.
 
 	function ready(error, datapoints) {
 
+
+	   defs.selectAll(".country-pattern")
+	    .data(datapoints)
+	    .enter().append("pattern")
+	    .attr("class", "country-pattern") 
+	    .attr("id", function(d) {
+	    	return d.Name.toLowerCase().replace(/ /g, "-")
+	    })
+	    .attr("height", "100%")
+	    .attr("width", "100%")
+	    .attr("patternContentUnits", "objectBoundingBox")
+	    .append("image")
+	    .attr("height", 1)
+	    .attr("width", 1)
+	    .attr("preserveAspectRatio", "none")
+	    .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+	    .attr("xlink:href", function(d) {
+	    	return d.Image_path
+	    }) 
+
 	  var circles = svg.selectAll(".Name")
 	    .data(datapoints)
 	    .enter().append("circle")
-	    .attr("class", "artist")
+	    .attr("class", "country")
 	    .attr("r", function(d) {
 	    	return radiusScale(d.GDP);
 	    })
-	    .attr("fill", "lightblue")
+	    .attr("fill", function (d) {
+	    	return "url(#" + d.Name.toLowerCase().replace(/ /g, "-") + ")"
+	    })
+	    .on('click', function(d) {
+	    	console.log(d)
+	    })
 
 	  simulation.nodes(datapoints)
 	    .on('tick', ticked)
